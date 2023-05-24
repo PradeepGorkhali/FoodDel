a23298ce2b49523ef397c1aebaf3c915c3bfa32f
Pradeep Adhikari <adhikaripradeep11@gmail.com>
2023-05-24 09:31:15 +0545

    first commit

--- frontend/src/apis/pizza.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://riocafe.onrender.com',
-});
-
-//"start": "node ./backend/server.js",
-//"start": "nodemon --watch backend --exec node --experimental-modules backend/server.js"
